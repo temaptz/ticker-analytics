91256bba3ee5bc2a2d99788c8493491cdea67248
Artem Urvanov <temaptz@gmail.com>
2022-09-19 21:21:50 +0300

    develop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'ticker-analytics';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('canvasElement')
+  private canvasElement?: ElementRef<HTMLCanvasElement>;
+
+  ngAfterViewInit() {
+    this.initChart();
+  }
+
+  private initChart(): void {
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+    };
+
+    if (this.canvasElement) {
+      const chart = new Chart(
+        this.canvasElement?.nativeElement,
+        {
+          type: 'line',
+          data: data,
+          options: {}
+        }
+      );
+
+      console.log('CHART CREATED', chart);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgChartsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
